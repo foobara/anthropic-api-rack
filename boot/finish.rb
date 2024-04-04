7b1023d21afaa7cbd88789dafe9c5cf4378db9e2
Miles Georgi <azimux@gmail.com>
2024-04-04 00:38:50 +0000

    Initial commit

---
+++ boot/finish.rb
@@ -0,0 +1,3 @@
+require "foobara/anthropic_api_rack"
+
+Dir["#{__dir__}/*.rb"].each { |f| require f }
---
+++ spec/foobara/anthropic_api_rack_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Foobara::AnthropicApiRack do
+  it "has a version number" do
+    expect(Foobara::AnthropicApiRack::VERSION).to_not be_nil
+  end
+end
